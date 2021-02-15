ea70e956fe95b86cd4cd0e500175f68044c63be0
Thomas <thomas@hoermi.com>
2021-02-15 21:54:46 +0100

    Remove unused files

--- server/data/static/component/layout.js
+++ server/data/static/component/layout.js
@@ -1,12 +1,8 @@
 'use strict'
 import { h, Fragment } from '/js/preact.js'
 import { useState } from '/js/hooks.js'
-import { Menu } from '/component/menu.js'
-import { Text } from '/component/text.js'
 
 function Layout(props){
-	const [serach,setSearch] = useState('')
-	const quickSearch = {get:()=>serach, set:setSearch}
 	let titleIcon = h('img',{className:'menu-icon',src:'/assets/menu.png'})
 	if (props.backButton && window.history.length>1){
 		titleIcon = h('img',{className:'header-back',src:'/assets/back.png',onClick:()=>window.history.back()})
@@ -14,11 +10,9 @@ function Layout(props){
 	return h(Fragment,null,
 			h('div',{className:'header'},
 					h('div',null,titleIcon,props.title),
-					//h(Text,{property:quickSearch,type:'text',placeholder:'Quick Search'}),
 					h('div',null,'Profile'),
 					),
 			h('div',{className:'body'},
-					//h('div',{className:'menu'},h(Menu)),
 					h('div',{className:'content-wrapper'},props.children)
 			),
 			h('div',{className:'footer'})
