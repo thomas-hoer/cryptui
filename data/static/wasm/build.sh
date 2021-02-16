024fa37df0406fbc10025ee29093a1742de38b38
Thomas <Thomas@WIN-CUPQRO42002.fritz.box>
2021-02-16 17:50:47 +0100

    reduce code duplication in wasm source

---
+++ data/static/wasm/build.sh
@@ -0,0 +1,1 @@
+GOARCH=wasm GOOS=js go build -o main.wasm -ldflags "-s" -trimpath
--- data/static/wasm/main.go
+++ data/static/wasm/main.go
@@ -25,11 +25,10 @@ func createKey(this js.Value, args []js.Value) interface{} {
 	getRsaKey()
 	return nil
 }
-func encryptString(this js.Value, args []js.Value) interface{} {
+
+func encryptData(secretMessage []byte) map[string]interface{} {
 	rng := rand.Reader
 	label := []byte("orders")
-
-	secretMessage := []byte(args[0].String())
 	rsaKey := getRsaKey()
 
 	aesKey := make([]byte, 16)
@@ -48,36 +47,23 @@ func encryptString(this js.Value, args []js.Value) interface{} {
 
 	return result
 }
-func encrypt(this js.Value, args []js.Value) interface{} {
-	rng := rand.Reader
-	label := []byte("orders")
 
+func encryptString(this js.Value, args []js.Value) interface{} {
+	secretMessage := []byte(args[0].String())
+	return encryptData(secretMessage)
+}
+
+func encrypt(this js.Value, args []js.Value) interface{} {
 	input := args[0]
 	size := input.Length()
 	secretMessage := make([]byte, size)
 	js.CopyBytesToGo(secretMessage, input)
-	rsaKey := getRsaKey()
-
-	aesKey := make([]byte, 16)
-	nonce := make([]byte, 12)
-	rand.Read(aesKey)
-	rand.Read(nonce)
-	block, _ := aes.NewCipher(aesKey)
-	aesGCM, _ := cipher.NewGCM(block)
-	ciphertext := aesGCM.Seal(nil, nonce, secretMessage, nil)
-
-	ciphertextKey, _ := rsa.EncryptOAEP(sha256.New(), rng, &rsaKey.PublicKey, aesKey, label)
-	result := make(map[string]interface{})
-	result["data"] = base64.StdEncoding.EncodeToString(ciphertext)
-	result["key"] = base64.StdEncoding.EncodeToString(ciphertextKey)
-	result["nonce"] = base64.StdEncoding.EncodeToString(nonce)
-
-	return result
+	return encryptData(secretMessage)
 }
-func decryptToString(this js.Value, args []js.Value) interface{} {
+
+func decrypt(input js.Value) []byte {
 	rng := rand.Reader
 	label := []byte("orders")
-	input := args[0]
 	ciphertext, _ := base64.StdEncoding.DecodeString(input.Get("data").String())
 	ciphertextKey, _ := base64.StdEncoding.DecodeString(input.Get("key").String())
 	nonce, _ := base64.StdEncoding.DecodeString(input.Get("nonce").String())
@@ -86,26 +72,19 @@ func decryptToString(this js.Value, args []js.Value) interface{} {
 	block, _ := aes.NewCipher(aesKey)
 	aesGCM, _ := cipher.NewGCM(block)
 	plain, _ := aesGCM.Open(nil, nonce, ciphertext, nil)
-	return string(plain)
+	return plain
+}
+
+func decryptToString(this js.Value, args []js.Value) interface{} {
+	return string(decrypt(args[0]))
 }
 
 func decryptToBase64(this js.Value, args []js.Value) interface{} {
-	rng := rand.Reader
-	label := []byte("orders")
-	input := args[0]
-	ciphertext, _ := base64.StdEncoding.DecodeString(input.Get("data").String())
-	ciphertextKey, _ := base64.StdEncoding.DecodeString(input.Get("key").String())
-	nonce, _ := base64.StdEncoding.DecodeString(input.Get("nonce").String())
-	rsaKey := getRsaKey()
-	aesKey, _ := rsa.DecryptOAEP(sha256.New(), rng, rsaKey, ciphertextKey, label)
-	block, _ := aes.NewCipher(aesKey)
-	aesGCM, _ := cipher.NewGCM(block)
-	plain, _ := aesGCM.Open(nil, nonce, ciphertext, nil)
-	return base64.StdEncoding.EncodeToString(plain)
+	return base64.StdEncoding.EncodeToString(decrypt(args[0]))
 }
 
 func getRsaKey() *rsa.PrivateKey {
-	key := Retrieve("pk")
+	key := retrieve("pk")
 	if key != nil {
 		data, _ := base64.StdEncoding.DecodeString(*key)
 		pk, _ := x509.ParsePKCS1PrivateKey(data)
@@ -116,20 +95,22 @@ func getRsaKey() *rsa.PrivateKey {
 		pkBase64 := base64.StdEncoding.EncodeToString(pkData)
 		pubData := x509.MarshalPKCS1PublicKey(&rsaKey.PublicKey)
 		pubBase64 := base64.StdEncoding.EncodeToString(pubData)
-		Store("pub", pubBase64)
-		Store("pk", pkBase64)
+		store("pub", pubBase64)
+		store("pk", pkBase64)
 		return rsaKey
 	}
 }
+
 func newKey() []byte {
 	key, _ := rsa.GenerateKey(rand.Reader, 2048)
 	return x509.MarshalPKCS1PrivateKey(key)
 }
-func Store(key string, val string) {
+
+func store(key string, val string) {
 	js.Global().Get("localStorage").Call("setItem", key, val)
 }
 
-func Retrieve(key string) *string {
+func retrieve(key string) *string {
 	val := js.Global().Get("localStorage").Call("getItem", key)
 	if val.Truthy() {
 		value := val.String()
